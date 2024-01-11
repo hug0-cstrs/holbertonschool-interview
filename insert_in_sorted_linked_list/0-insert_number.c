3064a0991d55499daecf21c09c0a62c95713bd7b
hug0-cstrs <hugo.casteras@outlook.fr>
2024-01-11 09:54:49 +0000

    remove

--- insert_in_sorted_linked_list/0-insert_number.c
+++ insert_in_sorted_linked_list/0-insert_number.c
@@ -5,40 +5,31 @@
  * @head: pointer to pointer of first node of listint_t list
  * @number: integer to be included in new node
  * Return: address of the new element or NULL if it fails
-*/
+ */
 
 listint_t *insert_node(listint_t **head, int number)
 {
     listint_t *new, *current;
 
-    /* Vérifier si head est NULL */
     if (head == NULL)
         return (NULL);
 
-    /* Alloue de la mémoire pour le nouveau nœud */
     new = malloc(sizeof(listint_t));
     if (new == NULL)
         return (NULL);
 
-    /* Assigne une valeur au nouveau nœud */
     new->n = number;
     new->next = NULL;
 
-    /* Définit current sur head */
     current = *head;
 
-    /* Si la liste est vide, faire du nouveau nœud la tête */
     if (*head == NULL)
         *head = new;
-
-    /* Si il y a un nouveau nœud, il doit être inséré au début */
     else if (current->n > number)
     {
         new->next = current;
         *head = new;
     }
-
-    /* Trouver le nœud après lequel le nouveau nœud doit être inséré */
     else
     {
         while (current->next != NULL && current->next->n < number)
@@ -47,6 +38,5 @@ listint_t *insert_node(listint_t **head, int number)
         current->next = new;
     }
 
-    /* Retourne l'adresse du nouveau nœud inséré */
-    return (new); 
+    return (new);
 }
