019c48f4d9a08392ccf5c1e026491d08f7105b6b
hug0-cstrs <hugo.casteras@outlook.fr>
2024-01-22 10:12:01 +0000

    update file

--- minimum_operations/0-minoperations.py
+++ minimum_operations/0-minoperations.py
@@ -12,17 +12,27 @@ def minOperations(n):
         Returns:
             number of operations (Copy & Paste) to reach n Hs
     """
-    operations = 0
-    summation = 1
-    carrier = 0
+    # Initialize total_operations to keep track of the total num of operations
+    total_operations = 0
+    # Initialize current_sum to 1 as we start with one 'H' character
+    copy_value = 0
+    # Initialize copy_value to keep track of the last copied value
+    current_sum = 1
 
-    while summation < n:
-        if n % summation == 0:  # Copy when summation is a multiple of n
-            carrier = summation
-            summation *= 2
-            operations += 1
+    # Loop until current_sum is less than n
+    while current_sum < n:
+        # If current_sum is a multiple of n
+        if n % current_sum == 0:
+            # Update copy_value with the value of current_sum
+            copy_value = current_sum
+            # Double the current_sum as we paste the copied value
+            current_sum *= 2
+            # Increment total_operations as copy operation is performed
+            total_operations += 1
         else:
-            summation += carrier
-        operations += 1  # Always paste
+            # Add the copy_value to current_sum as we paste the copied value
+            current_sum += copy_value
+        # Increment total_operations as paste operation is performed
+        total_operations += 1
 
-    return operations
+    return total_operations  # Return the total number of operations
