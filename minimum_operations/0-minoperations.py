7a0850a884131ee05273d56738a231fe74ce8cb6
hug0-cstrs <hugo.casteras@outlook.fr>
2024-01-22 09:52:56 +0000

    try to fix module documentation error

--- minimum_operations/0-minoperations.py
+++ minimum_operations/0-minoperations.py
@@ -3,52 +3,26 @@
 
 
 def minOperations(n):
+    """ Calculates the fewest number of operations needed
+        to result in exactly n H characters in the file
+
+        Args:
+            n: repetitions of H
+
+        Returns:
+            number of operations (Copy & Paste) to reach n Hs
     """
-    Calculates the fewest number of operations needed to result
-    in exactly n H characters in the file.
-    
-    Args:
-        n: repetitions of H
+    operations = 0
+    summation = 1
+    carrier = 0
 
-    Returns:
-        number of operations (Copy & Paste) to reach n Hs
-    """
-    # This is a function that takes an integer n as input.
+    while summation < n:
+        if n % summation == 0:  # Copy when summation is a multiple of n
+            carrier = summation
+            summation *= 2
+            operations += 1
+        else:
+            summation += carrier
+        operations += 1  # Always paste
 
-    if n <= 1:
-        return 0
-    # If n is less than or equal to 1, the function returns 0. This is because
-    # no operations are needed to get 1 H character.
-
-    count = 0
-    # Initialize a counter to keep track of the number of operations.
-
-    i = 2
-    # Initialize a variable i to 2. This will be used to divide n.
-
-    while i <= n:
-        # Start a while loop that continues as long as i is less than or equal
-        # to n.
-
-        if n % i == 0:
-            # If n is divisible by i,
-
-            count += i
-            # add i to the count. This is because we can get i H characters by
-            # copying the existing H character and pasting it i-1 times.
-
-            n = n / i
-            # Divide n by i. This is because we have reduced the problem to
-            # finding the minimum number of operations to get n/i H characters.
-
-            i = i - 1
-            # Decrement i by 1. This is to check if the new n is divisible by a
-            # smaller number.
-
-        i = i + 1
-        # Increment i by 1. This is to check if n is divisible by the next
-        # number.
-
-    return count
-    # Return the count. This is the minimum number of operations needed to get
-    # n H characters.
+    return operations
