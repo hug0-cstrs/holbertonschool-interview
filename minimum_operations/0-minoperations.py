8bcccacc4020c404015e6c8bc5b89fc9dd2b7d1f
hug0-cstrs <hugo.casteras@outlook.fr>
2024-01-22 10:15:49 +0000

    update file

--- minimum_operations/0-minoperations.py
+++ minimum_operations/0-minoperations.py
@@ -1,38 +1,33 @@
 #!/usr/bin/python3
-"""Module which contains minoperations function"""
+
+"""
+Minimum operations module
+Using 'prime factorization' approach
+Number of prime factors --> numbers of operators
+== operations to get n's 'H'
+"""
 
 
 def minOperations(n):
-    """ Calculates the fewest number of operations needed
-        to result in exactly n H characters in the file
-
-        Args:
-            n: repetitions of H
-
-        Returns:
-            number of operations (Copy & Paste) to reach n Hs
-    """
-    # Initialize total_operations to keep track of the total num of operations
-    total_operations = 0
-    # Initialize current_sum to 1 as we start with one 'H' character
-    copy_value = 0
-    # Initialize copy_value to keep track of the last copied value
-    current_sum = 1
-
-    # Loop until current_sum is less than n
-    while current_sum < n:
-        # If current_sum is a multiple of n
-        if n % current_sum == 0:
-            # Update copy_value with the value of current_sum
-            copy_value = current_sum
-            # Double the current_sum as we paste the copied value
-            current_sum *= 2
-            # Increment total_operations as copy operation is performed
-            total_operations += 1
-        else:
-            # Add the copy_value to current_sum as we paste the copied value
-            current_sum += copy_value
-        # Increment total_operations as paste operation is performed
-        total_operations += 1
-
-    return total_operations  # Return the total number of operations
+    '''
+    Method to compute the fewest number of operations
+    to reach exactly 'n' times 'H' characters in the file
+    Text editor can only do 'Paste' and 'Copy all' operations
+    '''
+    if not isinstance(n, int):
+        return 0
+    if n == 1:
+        return 0
+    ops = 0
+    # 2 smallest prime number
+    i = 2
+    while i <= n:
+        if n % i == 0:
+            # bam => find a prime factor
+            ops += i
+            n /= i
+            # n can't still be divisible by 'i', don't miss that
+            # e.g. 24 /2 = 12 still divisible by 2 so i-=1 i+=1 we check again
+            i -= 1
+        i += 1
+    return int(ops)
