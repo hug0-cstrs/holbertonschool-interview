3fc0709f9f25d5392feb9998215a71c1794c1f00
hug0-cstrs <hugo.casteras@outlook.fr>
2024-01-25 10:11:43 +0000

    update file

--- minimum_operations/0-minoperations.py
+++ minimum_operations/0-minoperations.py
@@ -12,28 +12,28 @@ def minOperations(n):
         Returns:
             number of operations (Copy & Paste) to reach n Hs
     """
-    # Initialize total_operations to keep track of the total num of operations
-    total_operations = 0
-    # Initialize current_sum to 1 as we start with one 'H' character
-    copy_value = 0
-    # Initialize copy_value to keep track of the last copied value
-    current_sum = 1
+    # Initialize the result and index variables
+    result = 0
+    index = 2
 
-    # Loop until current_sum is less than n
-    while current_sum < n:
+    # If n is less than 2, return 0 as no operations are needed
+    if n < 2:
+        return 0
 
-        # If current_sum is a multiple of n
-        if n % current_sum == 0:
-            # Update copy_value with the value of current_sum
-            copy_value = current_sum
-            # Double the current_sum as we paste the copied value
-            current_sum *= 2
-            # Increment total_operations as copy operation is performed
-            total_operations += 1
-        else:
-            # Add the copy_value to current_sum as we paste the copied value
-            current_sum += copy_value
-        # Increment total_operations as paste operation is performed
-        total_operations += 1
+    # Iterate from index 2 to n+1
+    while (index < n + 1):
 
-    return total_operations  # Return the total number of operations
+        # Check if the problem is evenly breakable
+        while n % index == 0:
+
+            # If so, add the number of smaller problems to the result
+            result += index
+
+            # Create the smaller problem needed to get to n
+            n /= index
+
+        # Increment the index to check the next number
+        index += 1
+
+    # Return the result of the operations
+    return result
