44773516cc5f87a836340b24192c7b965f6db963
hug0-cstrs <hugo.casteras@outlook.fr>
2024-02-12 17:01:10 +0000

    Refactor log generator script to improve readability and add comments

--- log_parsing/0-generator.py
+++ log_parsing/0-generator.py
@@ -4,12 +4,12 @@ import sys
 from time import sleep
 import datetime
 
-for i in range(10000):
-    sleep(random.random())
-    sys.stdout.write("{:d}.{:d}.{:d}.{:d} - [{}] \"GET /projects/260 HTTP/1.1\" {} {}\n".format(
+for i in range(10000): # 10k lines
+    sleep(random.random()) # random sleep to simulate a real-world scenario
+    sys.stdout.write("{:d}.{:d}.{:d}.{:d} - [{}] \"GET /projects/260 HTTP/1.1\" {} {}\n".format( # random IP, date, HTTP method and status code
         random.randint(1, 255), random.randint(1, 255), random.randint(1, 255), random.randint(1, 255),
-        datetime.datetime.now(),
-        random.choice([200, 301, 400, 401, 403, 404, 405, 500]),
-        random.randint(1, 1024)
+        datetime.datetime.now(), # current date and time
+        random.choice([200, 301, 400, 401, 403, 404, 405, 500]), # random status code
+        random.randint(1, 1024) # random file size
     ))
-    sys.stdout.flush()
+    sys.stdout.flush() # flushes the buffer
