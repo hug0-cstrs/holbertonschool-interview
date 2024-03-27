669d444ec4ee277e7039ee260aabea4ffb2de916
hug0-cstrs <hugo.casteras@outlook.fr>
2024-03-27 22:01:09 +0000

    update file

--- menger/0-menger.c
+++ menger/0-menger.c
@@ -11,12 +11,12 @@
 
 int One(int i, int j)
 {
-	while (i != 0 && j != 0) // While i and j are not 0
+	while (i != 0 && j != 0)
 	{
-		if (i % 3 == 1 && j % 3 == 1) // If i and j are 1
+		if (i % 3 == 1 && j % 3 == 1)
 			return (0);
 
-		i /= 3, j /= 3; // Divide i and j by 3
+		i /= 3, j /= 3;
 	}
 	return (1);
 }
@@ -32,15 +32,15 @@ void menger(int level)
 {
 	int i, j, limit;
 
-	if (level < 0) // If level is less than 0, return
+	if (level < 0)
 		return;
 
-	for (i = 0, limit = pow(3, level); i < limit; i++) // For i = 0, limit = 3^level, i++
+	for (i = 0, limit = pow(3, level); i < limit; i++)
 	{
 		for (j = 0; j < limit; j++)
 		{
-			One(i, j) == 1 ? printf("%c", '#') : printf("%c", ' '); // If One(i, j) is 1, print '#', else print ' '
+			One(i, j) == 1 ? printf("%c", '#') : printf("%c", ' ');
 		}
-		printf("\n"); // Print new line
+		printf("\n");
 	}
 }
