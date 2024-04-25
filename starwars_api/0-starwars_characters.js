6515c1a99fc3f0a73805e1925f3efd19ca30e011
hug0-cstrs <hugo.casteras@outlook.fr>
2024-04-25 21:17:38 +0200

    add comments to the code for a better understanding

--- starwars_api/0-starwars_characters.js
+++ starwars_api/0-starwars_characters.js
@@ -1,24 +1,24 @@
 #!/usr/bin/node
 
-const args = process.argv.slice(2);
+const args = process.argv.slice(2); // get arguments from command line input
 const URL = 'https://swapi-api.hbtn.io/api/films/' + args[0];
 const request = require('request');
 
-const getCharacterName = (link) => {
-  const characterName = new Promise((resolve, reject) => {
-    request(link, (error, response, body) => {
-      if (error) console.error('error:', error);
-      resolve(JSON.parse(body).name);
+const getCharacterName = (link) => { // get character name from link
+  const characterName = new Promise((resolve, reject) => { // create promise to return character name
+    request(link, (error, response, body) => { // request character name from link
+      if (error) console.error('error:', error); // print error if request fails
+      resolve(JSON.parse(body).name); // return character name in JSON format
     });
   });
   return characterName;
 };
 
-request(URL, async (error, response, body) => {
-  if (error) console.error('error:', error);
-  const characters = JSON.parse(body).characters;
-  for (let i = 0; i < characters.length; i++) {
-    const characterName = await getCharacterName(characters[i]);
-    console.log(characterName);
+request(URL, async (error, response, body) => { // request characters from URL and print character names
+  if (error) console.error('error:', error); // print error if request fails
+  const characters = JSON.parse(body).characters; // get characters from URL body response and parse to JSON format
+  for (let i = 0; i < characters.length; i++) { // loop through characters and print character names
+    const characterName = await getCharacterName(characters[i]); // get character name from link
+    console.log(characterName); // print character name
   }
 });
