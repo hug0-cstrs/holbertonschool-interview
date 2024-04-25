44c62893aedeb0e47ba5cb535958c529e6943fc7
hug0-cstrs <hugo.casteras@outlook.fr>
2024-04-25 21:07:28 +0200

    Fix error : "Strings must use singlequote"

--- starwars_api/0-starwars_characters.js
+++ starwars_api/0-starwars_characters.js
@@ -1,13 +1,13 @@
 #!/usr/bin/node
 
 const args = process.argv.slice(2);
-const URL = "https://swapi-api.hbtn.io/api/films/" + args[0];
-const request = require("request");
+const URL = 'https://swapi-api.hbtn.io/api/films/' + args[0];
+const request = require('request');
 
 const getCharacterName = (link) => {
   const characterName = new Promise((resolve, reject) => {
     request(link, (error, response, body) => {
-      if (error) console.error("error:", error);
+      if (error) console.error('error:', error);
       resolve(JSON.parse(body).name);
     });
   });
@@ -15,7 +15,7 @@ const getCharacterName = (link) => {
 };
 
 request(URL, async (error, response, body) => {
-  if (error) console.error("error:", error);
+  if (error) console.error('error:', error);
   const characters = JSON.parse(body).characters;
   for (let i = 0; i < characters.length; i++) {
     const characterName = await getCharacterName(characters[i]);
