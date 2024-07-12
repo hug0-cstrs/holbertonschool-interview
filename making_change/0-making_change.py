914ead20d723c66c5f5304a88c95c7cedcfa928e
hug0-cstrs <hugo.casteras@outlook.fr>
2024-07-12 11:52:48 +0200

    update file

--- making_change/0-making_change.py
+++ making_change/0-making_change.py
@@ -7,14 +7,14 @@ def makeChange(coins, total):
     Function that given a pile of coins of different values,
     determine the fewest number of coins needed to meet a given amount total
     """
-    if total < 1:  # if total is 0 or less
+    if total < 1:
         return 0
-    change = 0  # number of coins
-    coins.sort(reverse=True)  # sort coins in descending order
-    for coin in coins:  # iterate through coins
-        temp_change = int(total / coin)  # number of coins of particular value
-        total -= (temp_change * coin)  # subtract the value of coins from total
-        change += temp_change  # add the number of coins to change
+    change = 0
+    coins.sort(reverse=True)
+    for coin in coins:
+        temp_change = int(total / coin)
+        total -= (temp_change * coin)
+        change += temp_change
         if total == 0:
             return change
     if total != 0:
